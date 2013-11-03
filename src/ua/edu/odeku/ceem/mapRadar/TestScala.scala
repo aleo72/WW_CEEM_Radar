561557f3e6fd26499ea4be9d52ad030c0509a358
Алексей <72aleo72@gmail.com>
2013-11-03 13:38:42 +0200

    Подключена Scala

---
+++ src/ua/edu/odeku/ceem/mapRadar/TestScala.scala
@@ -0,0 +1,12 @@
+package ua.edu.odeku.ceem.mapRadar
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Aleo
+ * Date: 03.11.13
+ * Time: 13:34
+ * To change this template use File | Settings | File Templates.
+ */
+object TestScala extends App{
+  println("test")
+}
