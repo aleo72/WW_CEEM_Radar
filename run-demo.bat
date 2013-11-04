ff7cf6d6bc6c9544d3c7b703f77345b7d23ba62b
Алексей <72aleo72@gmail.com>
2013-11-04 21:46:47 +0200

    точка входа изменена, теперь CeemRadar можно запускать без среды работки

--- run-demo.bat
+++ run-demo.bat
@@ -6,4 +6,4 @@ REM Windows Batch file for Running a WorldWind Demo
 REM $Id: run-demo.bat 1171 2013-02-11 21:45:02Z dcollins $
 
 @echo Running %1
-java -Xmx512m -Dsun.java2d.noddraw=true -classpath .\src;.\classes;.\worldwind.jar;.\worldwindx.jar;.\jogl-all.jar;.\gluegen-rt.jar;.\gdal.jar gov.nasa.worldwindx.examples.ApplicationTemplate
+java -Xmx512m -Dsun.java2d.noddraw=true -classpath .\src;.\classes;.\worldwind.jar;.\worldwindx.jar;.\jogl-all.jar;.\gluegen-rt.jar;.\gdal.jar ua.edu.odeku.ceem.mapRadar.CeemRadarApplicationTemplate
