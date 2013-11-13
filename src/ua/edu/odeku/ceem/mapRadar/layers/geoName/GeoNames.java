069f7d393f259147360bf929bc0840a19a3058c7
Aleo <72aleo72@gmail.com>
2013-11-13 23:01:43 +0200

    Начало реализации слоя geoNames

---
+++ src/ua/edu/odeku/ceem/mapRadar/layers/geoName/GeoNames.java
@@ -0,0 +1,168 @@
+package ua.edu.odeku.ceem.mapRadar.layers.geoName;
+
+import gov.nasa.worldwind.geom.Angle;
+import gov.nasa.worldwind.geom.LatLon;
+import gov.nasa.worldwind.geom.Sector;
+import gov.nasa.worldwind.util.Logging;
+import gov.nasa.worldwind.util.Tile;
+
+import java.awt.*;
+
+/**
+ * User: Aleo skype: aleo72
+ * Date: 12.11.13
+ * Time: 22:56
+ */
+public class GeoNames {
+
+    private static final Sector TILING_SECTOR = Sector.FULL_SPHERE;
+    private static final int MAX_ABSENT_TILE_TRIES = 2;
+    private static final int MIN_ABSENT_TILE_CHECK_INTERVAL = 10000;
+
+    public final String geoNameClass;
+    public final String geoNameCode;
+
+    private final LatLon tileDelta;
+    private final Font font;
+
+    private boolean enabled;
+    private java.awt.Color color;
+    private java.awt.Color backgroundColor;
+    private double minDisplayDistance;
+    private double maxDisplayDistance;
+
+    private int numColumns;
+
+
+    public GeoNames(String geoNameClass, String geoNameCode, LatLon tileDelta, Font font) {
+        this.tileDelta = tileDelta;
+        this.geoNameClass = geoNameClass;
+        this.geoNameCode = geoNameCode;
+        this.font = font;
+
+        this.enabled = true;
+        this.color = Color.WHITE;
+        this.minDisplayDistance = Double.MIN_VALUE;
+        this.maxDisplayDistance = Double.MAX_VALUE;
+
+        String message = this.validate();
+        if (message != null)
+        {
+            Logging.logger().severe(message);
+            throw new IllegalArgumentException(message);
+        }
+
+        this.numColumns = this.numColumnsInLevel();
+    }
+
+    private int numColumnsInLevel() {
+        int firstCol = Tile.computeColumn(this.tileDelta.getLongitude(), TILING_SECTOR.getMinLongitude(), Angle.NEG180);
+        int lastCol = Tile.computeColumn(this.tileDelta.getLongitude(), TILING_SECTOR.getMaxLongitude().subtract(this.tileDelta.getLongitude()), Angle.NEG180);
+
+        return lastCol - firstCol + 1;
+    }
+
+    private String validate() {
+        String msg = "";
+        if (this.geoNameClass == null)
+        {
+            msg += Logging.getMessage("nullValue.GeoNameClassIsNull") + ", ";
+        }
+        if (this.geoNameCode == null)
+        {
+            msg += Logging.getMessage("nullValue.GeoNameCodeIsNull") + ", ";
+        }
+        if (this.tileDelta == null)
+        {
+            msg += Logging.getMessage("nullValue.TileDeltaIsNull") + ", ";
+        }
+        if (this.font == null)
+        {
+            msg += Logging.getMessage("nullValue.FontIsNull") + ", ";
+        }
+
+        if (msg.length() == 0)
+        {
+            return null;
+        }
+
+        return msg;
+    }
+
+    public final GeoNames copy(){
+        GeoNames copy = new GeoNames(this.geoNameClass, this.geoNameCode, this.tileDelta, this.font);
+        return copy;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public Color getBackgroundColor() {
+        if (this.backgroundColor == null)
+            this.backgroundColor = suggestBackgroundColor(this.color);
+        return this.backgroundColor;
+    }
+
+    private Color suggestBackgroundColor(Color foreground) {
+        float[] compArray = new float[4];
+        Color.RGBtoHSB(foreground.getRed(), foreground.getGreen(), foreground.getBlue(), compArray);
+        int colorValue = compArray[2] < 0.5f ? 255 : 0;
+        int alphaValue = foreground.getAlpha();
+        return new Color(colorValue, colorValue, colorValue, alphaValue);
+    }
+
+    public void setBackgroundColor(Color backgroundColor) {
+        this.backgroundColor = backgroundColor;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public double getMinDisplayDistance() {
+        return minDisplayDistance;
+    }
+
+    public void setMinDisplayDistance(double minDisplayDistance) {
+        this.minDisplayDistance = minDisplayDistance;
+    }
+
+    public double getMaxDisplayDistance() {
+        return maxDisplayDistance;
+    }
+
+    public void setMaxDisplayDistance(double maxDisplayDistance) {
+        this.maxDisplayDistance = maxDisplayDistance;
+    }
+
+    public int getNumColumns() {
+        return numColumns;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || this.getClass() != o.getClass())
+            return false;
+
+        final GeoNames other = (GeoNames) o;
+
+        if(this.geoNameClass != null ? !this.geoNameClass.equals(other.geoNameClass) : other.geoNameClass != null){
+            return false;
+        }
+        if (this.geoNameCode != null ? !this.geoNameCode.equals(other.geoNameCode) : other.geoNameCode != null){
+            return false;
+        }
+        return true;
+    }
+}
---
+++ src/ua/edu/odeku/ceem/mapRadar/layers/geoName/GeoNamesSet.java
@@ -0,0 +1,74 @@
+package ua.edu.odeku.ceem.mapRadar.layers.geoName;
+
+import gov.nasa.worldwind.WWObjectImpl;
+import gov.nasa.worldwind.avlist.AVList;
+import gov.nasa.worldwind.util.Logging;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * User: Aleo skype: aleo72
+ * Date: 12.11.13
+ * Time: 22:57
+ */
+public class GeoNamesSet extends WWObjectImpl {
+
+    private final List<GeoNames> geoNamesList = new ArrayList<GeoNames>(20);
+
+    public void addGeoNames(GeoNames geoNames, boolean replace){
+
+        if(geoNames != null){
+            for(int i = 0; i < this.geoNamesList.size(); i++){
+                final GeoNames other = this.geoNamesList.get(i);
+                if(geoNames.geoNameClass.equals(other.geoNameClass) && geoNames.geoNameCode.equals(other.geoNameCode)){
+                    if(replace){
+                        this.geoNamesList.set(i, geoNames);
+                        return;
+                    } else {
+                        return;
+                    }
+                }
+            }
+
+            this.geoNamesList.add(geoNames);
+        } else {
+            String message = Logging.getMessage("nullValue.GeoNamesIsNull");
+            Logging.logger().severe(message);
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+    public GeoNamesSet copy(){
+        GeoNamesSet copy = new GeoNamesSet();
+        copy.setValues(this);
+
+        for (int i = 0; i < this.geoNamesList.size(); i++){
+            copy.geoNamesList.add(i, this.geoNamesList.get(i).copy());
+        }
+
+        return copy;
+    }
+
+    public final int getGeoNamesSetSize(){
+        return this.geoNamesList.size();
+    }
+
+    public final GeoNames getGeoNames(int index){
+        return this.geoNamesList.get(index);
+    }
+
+    public List<GeoNames> getGeoNamesList(){
+        return geoNamesList;
+    }
+
+    public GeoNames getGeoNames(String class_code){
+
+        for (GeoNames geoNames : this.geoNamesList) {
+            if ((geoNames.geoNameClass + "_" + geoNames.geoNameCode).equalsIgnoreCase(class_code)) {
+                return geoNames;
+            }
+        }
+        return null;
+    }
+}
