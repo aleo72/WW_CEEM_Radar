ba78ced83ac01fcabdd60f9b87967e1a11a3170f
Алексей <72aleo72@gmail.com>
2013-12-13 17:37:46 +0200

    Начало реализации RadarManagerTool

--- src/ua/edu/odeku/ceem/mapRadar/tools/radarManager/RadarManagerTool.scala
+++ src/ua/edu/odeku/ceem/mapRadar/tools/radarManager/RadarManagerTool.scala
@@ -5,11 +5,19 @@
 
 package ua.edu.odeku.ceem.mapRadar.tools.radarManager
 
+import ua.edu.odeku.ceem.mapRadar.tools.{ToolFrame, CeemRadarTool}
+import javax.swing.JPanel
+
 /**
  * User: Aleo Bakalov
  * Date: 11.12.13
  * Time: 17:34
  */
-class RadarManagerTool {
+class RadarManagerTool extends CeemRadarTool {
 
+  def startFunction: (ToolFrame) => Unit = ???
+
+  def endFunction: (ToolFrame) => Unit = ???
+
+  def rootPanel: JPanel = ???
 }
---
+++ src/ua/edu/odeku/ceem/mapRadar/tools/radarManager/panel/RadarEditorForm.java
@@ -0,0 +1,22 @@
+/*
+ * Odessa State environmental University
+ * Copyright (C) 2013
+ */
+
+package ua.edu.odeku.ceem.mapRadar.tools.radarManager.panel;
+
+import javax.swing.*;
+
+/**
+ * User: Aleo Bakalov
+ * Date: 13.12.13
+ * Time: 13:44
+ */
+public class RadarEditorForm {
+    private JPanel panel1;
+    private JSpinner transmotterPowerSpinner;
+    private JSpinner AntenaGainSpinner;
+    private JSpinner EffectiveAreaSpinner;
+    private JSpinner scatteringCrossSectionSpinner;
+    private JSpinner minimumReceiverSensitivitySpinner;
+}
