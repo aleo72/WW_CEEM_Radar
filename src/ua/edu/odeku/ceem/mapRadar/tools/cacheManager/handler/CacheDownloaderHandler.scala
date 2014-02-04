2afecb0161cdd976494a21648b7b9b9181112165
Алексей <72aleo72@gmail.com>
2014-02-04 17:09:07 +0200

    Продолжение реализации

--- src/ua/edu/odeku/ceem/mapRadar/tools/cacheManager/handler/CacheDownloaderHandler.scala
+++ src/ua/edu/odeku/ceem/mapRadar/tools/cacheManager/handler/CacheDownloaderHandler.scala
@@ -10,15 +10,15 @@ import gov.nasa.worldwind.WorldWindow
 import gov.nasa.worldwind.geom.Sector
 import scala.collection.mutable.ArrayBuffer
 import javax.swing._
-import gov.nasa.worldwind.retrieve.{BulkRetrievalThread, BulkRetrievable}
+import gov.nasa.worldwind.retrieve.{Progress, BulkRetrievalThread, BulkRetrievable}
 import gov.nasa.worldwind.cache.BasicDataFileStore
 import gov.nasa.worldwind.layers.Layer
 import gov.nasa.worldwind.terrain.CompoundElevationModel
 import gov.nasa.worldwind.globes.ElevationModel
-import java.awt.{Component, Color}
+import java.awt._
 import java.beans.{PropertyChangeEvent, PropertyChangeListener}
 import java.util.ResourceBundle
-import java.awt.event.ActionEvent
+import java.awt.event.{ActionListener, ActionEvent}
 import gov.nasa.worldwind.event.{BulkRetrievalEvent, BulkRetrievalListener}
 import ua.edu.odeku.ceem.mapRadar.utils.sector.{SectorUtils, SectorSelector}
 import gov.nasa.worldwindx.examples.BulkDownloadPanel
@@ -68,7 +68,7 @@ class CacheDownloaderHandler(val form: CacheDownloaderForm, val wwd: WorldWindow
 	def updateSector() {
 		this.currentSector = this.selector.sector
 		if (this.currentSector != null) {
-			this.form.sectorSelectedTextField.setText(makeSectorDescription(this.currentSector))
+			this.form.sectorSelectedTextField.setText(SectorUtils.makeSectorDescription(this.currentSector))
 			this.form.selectButton.setText(ResourceBundle.getBundle("button").getString("clear-sector"))
 			this.form.startButton.setEnabled(true)
 		} else {
@@ -95,21 +95,21 @@ class CacheDownloaderHandler(val form: CacheDownloaderForm, val wwd: WorldWindow
 
 	/** Clear the current selection sector and remove it from the globe. */
 	def clearSector() {
-		if(this.selector.sector != null){
+		if (this.selector.sector != null) {
 			this.selector.disable()
 		}
 		updateSector()
 	}
 
-	def startButtonActionPerformed(event: ActionEvent){
-		for(panel: BulkRetrievablePanel <- this.retrievables if panel.selectCheckBox.isSelected){
+	def startButtonActionPerformed(event: ActionEvent) {
+		for (panel: BulkRetrievablePanel <- this.retrievables if panel.selectCheckBox.isSelected) {
 			val retrievable = panel.retrievable
 			val thread = retrievable.makeLocal(this.currentSector, 0, this.cache, new BulkRetrievalListener {
 				override def eventOccurred(event: BulkRetrievalEvent): Unit = {
 
 				}
 			})
-			if (thread != null){
+			if (thread != null) {
 				this.form.monitorPanel.add(new DownloadMonitorPanel(thread))
 			}
 		}
@@ -118,19 +118,19 @@ class CacheDownloaderHandler(val form: CacheDownloaderForm, val wwd: WorldWindow
 
 	def hasActiveDownloads: Boolean = {
 		var flag = false
-		for(c: Component <- this.form.monitorPanel.getComponents
-		    if !flag && c.isInstanceOf[DownloadMonitorPanel] && c.asInstanceOf[DownloadMonitorPanel].thread.isAlive){
+		for (c: Component <- this.form.monitorPanel.getComponents
+			 if !flag && c.isInstanceOf[DownloadMonitorPanel] && c.asInstanceOf[DownloadMonitorPanel].thread.isAlive) {
 			flag = true
 		}
 		flag
 	}
 
-	def cancelActiveDownloads(){
-		for(c:Component <- this.form.monitorPanel.getComponents
-		    if c.isInstanceOf[DownloadMonitorPanel] && c.asInstanceOf[DownloadMonitorPanel].thread.isAlive){
+	def cancelActiveDownloads() {
+		for (c: Component <- this.form.monitorPanel.getComponents
+			 if c.isInstanceOf[DownloadMonitorPanel] && c.asInstanceOf[DownloadMonitorPanel].thread.isAlive) {
 			val panel = c.asInstanceOf[DownloadMonitorPanel]
 			panel.cancelButtonActionPerformed(null)
-			try{
+			try {
 				val time = System.currentTimeMillis()
 				while (panel.thread.isAlive && System.currentTimeMillis() - time < 500)
 					Thread.sleep(10)
@@ -141,13 +141,13 @@ class CacheDownloaderHandler(val form: CacheDownloaderForm, val wwd: WorldWindow
 		}
 	}
 
-	def clearInactiveDownloads(){
+	def clearInactiveDownloads() {
 		val count: Int = this.form.monitorPanel.getComponentCount - 1
-		for( i <- count.to(0, -1) ){
+		for (i <- count.to(0, -1)) {
 			val c: Component = this.form.monitorPanel.getComponent(i)
 			c match {
-				case panel:DownloadMonitorPanel =>
-					if(!panel.thread.isAlive || panel.thread.isInterrupted){
+				case panel: DownloadMonitorPanel =>
+					if (!panel.thread.isAlive || panel.thread.isInterrupted) {
 						this.form.monitorPanel.remove(i)
 					}
 			}
@@ -161,13 +161,28 @@ class CacheDownloaderHandler(val form: CacheDownloaderForm, val wwd: WorldWindow
 
 	protected class BulkRetrievablePanel(val retrievable: BulkRetrievable) extends JPanel {
 
-		val selectCheckBox: JCheckBox = new JCheckBox()
-		val descriptionLabel: JLabel = null
+		var selectCheckBox: JCheckBox = new JCheckBox()
+		var descriptionLabel: JLabel = null
 		var updateThread: Thread = null
 		var sector: Sector = null
 
 		def initComponents() {
-			// TODO ---!!!!!!!!!!!!!!!!!!!!
+			this.setLayout(new BorderLayout())
+			this.setBorder(BorderFactory.createEmptyBorder(1, 1, 1, 1))
+
+			this.selectCheckBox = new JCheckBox(this.retrievable.getName)
+			this.selectCheckBox.addActionListener(new ActionListener {
+				override def actionPerformed(e: ActionEvent): Unit = {
+					if (e.getSource.asInstanceOf[JCheckBox].isSelected && sector != null) {
+						updateDescription(sector)
+					}
+				}
+			})
+
+			this.add(this.selectCheckBox, BorderLayout.WEST)
+
+			this.descriptionLabel = new JLabel()
+			this.add(this.descriptionLabel, BorderLayout.EAST)
 		}
 
 		def updateDescription(sector: Sector) {
@@ -175,7 +190,7 @@ class CacheDownloaderHandler(val form: CacheDownloaderForm, val wwd: WorldWindow
 				return
 
 			this.sector = sector
-			if(!this.selectCheckBox.isSelected){
+			if (!this.selectCheckBox.isSelected) {
 				doUpdateDescription(null)
 			} else {
 				this.updateThread = new Thread(new Runnable {
@@ -188,9 +203,9 @@ class CacheDownloaderHandler(val form: CacheDownloaderForm, val wwd: WorldWindow
 			}
 		}
 
-		def doUpdateDescription(sector: Sector){
-			if(sector != null){
-				try{
+		def doUpdateDescription(sector: Sector) {
+			if (sector != null) {
+				try {
 					val size = retrievable.getEstimatedMissingDataSize(sector, 0, cache)
 					val formattedSize = SectorUtils.makeSizeDescription(size)
 					SwingUtilities.invokeLater(new Runnable {
@@ -217,8 +232,87 @@ class CacheDownloaderHandler(val form: CacheDownloaderForm, val wwd: WorldWindow
 
 		override def toString = this.retrievable.getName
 	}
+
 }
 
-class DownloadMonitorPanel(val thread: BulkRetrievalThread) extends JPanel{
+class DownloadMonitorPanel(val thread: BulkRetrievalThread) extends JPanel {
 
+	this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS))
+	this.setBorder(BorderFactory.createEmptyBorder(4, 4, 4, 4))
+
+	val progress: Progress = thread.getProgress
+
+	val descriptionPanel = new JPanel(new GridLayout(0, 1, 0, 0))
+	descriptionPanel.setBorder(BorderFactory.createEmptyBorder(2, 2, 2, 2))
+
+	val descriptionLabel: JLabel = new JLabel(if (this.thread.getRetrievable.getName.length > 27) this.thread.getRetrievable.getName.substring(0, 40) + "..." else this.thread.getRetrievable.getName)
+	descriptionPanel.add(descriptionLabel)
+
+	this.add(descriptionPanel)
+
+	val progressPanel = new JPanel(new BoxLayout(progressPanel, BoxLayout.X_AXIS))
+	progressPanel.setBorder(BorderFactory.createEmptyBorder(2, 2, 2, 2))
+
+	val progressBar: JProgressBar = new JProgressBar(0, 100)
+	progressBar.setPreferredSize(new Dimension(100, 16))
+	progressPanel.add(progressBar)
+	progressPanel.add(Box.createHorizontalStrut(8))
+
+	val cancelButton: JButton = new JButton(ResourceBundle.getBundle("button").getString("cancel"))
+	cancelButton.setBackground(Color.RED)
+	cancelButton.addActionListener(new ActionListener {
+		override def actionPerformed(e: ActionEvent): Unit = {
+			cancelButtonActionPerformed(e)
+		}
+	})
+
+	progressPanel.add(cancelButton)
+	this.add(progressPanel)
+
+	val updateTimer: Timer = new Timer(1000, new ActionListener {
+		override def actionPerformed(e: ActionEvent): Unit = {
+			updateStatus()
+		}
+	})
+
+	this.updateTimer.start()
+
+	def updateStatus() {
+		var text = if (this.thread.getRetrievable.getName.length > 27) this.thread.getRetrievable.getName.substring(0, 27) + "..." else this.thread.getRetrievable.getName
+		text += " (" + SectorUtils.makeSizeDescription(this.progress.getCurrentSize) + " / " + SectorUtils.makeSizeDescription(this.progress.getTotalSize) + ")"
+		this.descriptionLabel.setText(text)
+		val percent: Double = if (this.progress.getTotalCount > 0) this.progress.getCurrentCount / this.progress.getTotalCount else 0
+		this.progressBar.setValue(Math.min(percent.toInt, 100))
+
+		descriptionLabel.setToolTipText(SectorUtils.makeSectorDescription(this.thread.getSector))
+		this.progressBar.setToolTipText(makeProgressDescription())
+
+		if (!this.thread.isAlive) {
+			this.cancelButton.setText(ResourceBundle.getBundle("button").getString("remove"))
+			this.cancelButton.setBackground(Color.GREEN)
+			this.updateTimer.stop()
+		}
+	}
+
+	def cancelButtonActionPerformed(event: ActionEvent) {
+		if (this.thread.isAlive) {
+			this.thread.interrupt()
+			this.cancelButton.setBackground(Color.ORANGE)
+			this.cancelButton.setText(ResourceBundle.getBundle("button").getString("remove"))
+			this.updateTimer.stop()
+		} else {
+			val top = this.getTopLevelAncestor
+			this.getParent.remove(this)
+			top.validate()
+		}
+	}
+
+	def makeProgressDescription(): String = {
+		if (this.progress.getTotalCount > 0) {
+			val percent = this.progress.getCurrentSize / this.progress.getTotalSize * 100.0
+			percent.toInt + "% (" + SectorUtils.makeSizeDescription(this.progress.getTotalSize) + ")"
+		} else {
+			""
+		}
+	}
 }
