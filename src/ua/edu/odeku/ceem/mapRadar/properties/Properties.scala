c3dc9418a77cdb5ece1e40f4e648ecaec81dc5e7
Aleo Bakalov <aleo.bakalov@gmail.com>
2014-08-02 20:41:29 +0300

    Створені нові налаштування

---
+++ src/ua/edu/odeku/ceem/mapRadar/properties/Properties.scala
@@ -0,0 +1,8 @@
+package ua.edu.odeku.ceem.mapRadar.properties
+
+/**
+ * Created by aleo on 02.08.14.
+ */
+class Properties {
+
+}
---
+++ src/ua/edu/odeku/ceem/mapRadar/settings/SettingsProgram.scala
@@ -0,0 +1,8 @@
+package ua.edu.odeku.ceem.mapRadar.settings
+
+/**
+ * Created by aleo on 02.08.14.
+ */
+object SettingsProgram {
+
+}
